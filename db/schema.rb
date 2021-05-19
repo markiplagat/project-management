fe428a9ec6d01c20ce4967969d3eac2854042a65
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-05-20 00:09:48 +0300

    Add project scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_205152) do
+ActiveRecord::Schema.define(version: 2021_05_19_210123) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "team_id"
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"

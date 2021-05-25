77d9d2a13076bd711cbad59926216c0e1c062173
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-05-25 23:19:03 +0300

    Fixes

--- app/javascript/packs/project.js
+++ app/javascript/packs/project.js
@@ -1,24 +1,25 @@
-import Vue from 'vue/dist/vue.esm'
+import Vue from 'vue/dist/vue.js'
 import VueResource from 'vue-resource'
 
-Vue.user(VueResource)
+
+Vue.use(VueResource)
 
 document.addEventListener('turbolinks:load', () => {
-    Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').get
-    Attribute('content')
-  var element = document.getElementById('team-form')
+    Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 
-    if(element != null) {
+    var element = document.getElementById("team-form")
+    if (element != null) {
+
         var id = element.dataset.id
         var team = JSON.parse(element.dataset.team)
-        var user_attributes = JSON.parse(element.data.users_attributes)
-        users_attributes.forEach(function (user) { user._destroy = null})
+        var users_attributes = JSON.parse(element.dataset.usersAttributes)
+        users_attributes.forEach(function(user) { user._destroy = null })
         team.users_attributes = users_attributes
 
         var app = new Vue({
             el: element,
-            data: function () {
-                return{
+            data: function() {
+                return {
                     id: id,
                     team: team,
                     errors: [],
@@ -27,7 +28,7 @@ document.addEventListener('turbolinks:load', () => {
             },
             mounted() {
                 window.addEventListener('scroll', this.updateScroll);
-            }
+            },
             methods: {
                 updateScroll() {
                     this.scrollPosition = window.scrollY
@@ -37,35 +38,42 @@ document.addEventListener('turbolinks:load', () => {
                         id: null,
                         name: "",
                         email: "",
+                        //position: "",
                         _destroy: null
                     })
                 },
+
                 removeUser: function(index) {
                     var user = this.team.users_attributes[index]
 
-                    if (user.id == null ) {
+                    if (user.id == null) {
                         this.team.users_attributes.splice(index, 1)
                     } else {
-                        this.team.users_attributes._destroy = "1"
+                        this.team.users_attributes[index]._destroy = "1"
                     }
                 },
-                undoRemove: function() {
+
+                undoRemove: function(index) {
                     this.team.users_attributes[index]._destroy = null
                 },
+
                 saveTeam: function() {
-                    if(this.id == null) {
-                        this.$http.post('/teams', { team: this.team}).then(response => {
+                    // Create a new team
+
+                    if (this.id == null) {
+                        this.$http.post('/teams', { team: this.team }).then(response => {
                             Turbolinks.visit(`/teams/${response.body.id}`)
                         }, response => {
                             console.log(response)
 
-                            if (response.status == 422) {
+                            if (response.status = 422) {
                                 var json = JSON.parse(response.bodyText);
-                                this.errors = json["user.email"][0]
+                                this.errors = json["users.email"][0];
                             }
                         })
+                        // Edit an existing team
                     } else {
-                        this.$http.put(`/teams/${this.id}`, {team: this.team}).then(response =>  {
+                        this.$http.put(`/teams/${this.id}`, { team: this.team }).then(response => {
                             Turbolinks.visit(`/teams/${response.body.id}`)
                         }, response => {
                             console.log(response)
